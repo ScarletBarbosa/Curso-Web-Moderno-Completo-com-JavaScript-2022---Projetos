c04a084ccf99d6545494c76ea9f3c212d0a2ad62
Scarlet Barbosa <scarlet.profissional@gmail.com>
2023-04-28 11:47:38 -0300

    Create aula83-estrutura-do-while.js
    Aplicação da Estrutura Do/While

---
+++ Secao_5-Estruturas_de_Controle/aula83-estrutura-do-while.js
@@ -0,0 +1,20 @@
+/**
+ * Estrutura Switch
+ */
+/**
+ * Estrutura do/while
+ */
+
+function getInteiroAleatorioEntre(min, max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+let opcao = -1
+
+do {
+    opcao = getInteiroAleatorioEntre(-1, 10)
+    console.log(`Opção escolhida foi ${opcao}.`)
+} while (opcao != -1)
+
+console.log('Até a próxima!')
